1837dc78c96807f861cd0abcc7351efc93fcd98a
Peter Luo <zl2005.luo@icloud.com>
2020-10-13 01:41:39 +0800

    More flexibility when adding new objects

--- GravitySimulator/GameScene.swift
+++ GravitySimulator/GameScene.swift
@@ -10,57 +10,65 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    let userCamera = SKCameraNode()
+    private let userCamera = SKCameraNode()
+    
+    private var newlyAddedObject : Object?
+    
+    private var userIsAddingObject = false {
+        didSet {
+            if userIsAddingObject {
+                self.physicsWorld.speed = 0
+            } else {
+                self.physicsWorld.speed = 1
+                newlyAddedObject = nil
+                self.childNode(withName: "velIndicator")?.removeFromParent()
+            }
+        }
+    }
     
     override func sceneDidLoad() {
         initializeScene()
     }
     
     override func keyDown(with event: NSEvent) {
-        print("Key pressed down: \(event.keyCode)")
+        print(event.keyCode)
         switch Int(event.keyCode) {
         case 24: // =
-            guard userCamera.xScale > 0 && userCamera.yScale > 0 else { return }
-            userCamera.xScale -= 1
-            userCamera.yScale -= 1
+            guard userCamera.xScale > 1 && userCamera.yScale > 1 else { return }
+            userCamera.xScale -= 0.5
+            userCamera.yScale -= 0.5
         case 27: // -
             userCamera.xScale += 1
             userCamera.yScale += 1
-        case 47: // .
-            self.physicsWorld.speed += 0.5
-        case 43: // ,
-            guard self.physicsWorld.speed > 0 else { return }
-            self.physicsWorld.speed -= 0.5
-        case 126: // up
-            userCamera.position.y += 20
-        case 125: // down
-            userCamera.position.y -= 20
-        case 123: // left
-            userCamera.position.x -= 20
-        case 124: // right
-            userCamera.position.x += 20
         case 35: // p
             self.isPaused.toggle()
-        case 0: // a
-            addRandomObject()
         case 15: // r
             initializeScene()
         default:
             break
         }
-        self.camera = userCamera
     }
     
-    private var objects: [Object] {
-        var results = [Object]()
-        for node in self.children {
-            if node.name == "object",
-               let shapeNode = node as? Object {
-                results.append(shapeNode)
-            }
-        }
-        return results
+    override func mouseDown(with event: NSEvent) {
+        userAddObject()
+        newlyAddedObject?.position = event.location(in: self)
     }
+    
+    override func mouseDragged(with event: NSEvent) {
+        let mousePos = event.location(in: self)
+        newlyAddedObject?.physicsBody?.velocity = CGVector(
+            dx: mousePos.x - newlyAddedObject!.position.x,
+            dy: mousePos.y - newlyAddedObject!.position.y
+        )
+        updateVelocityIndicator()
+    }
+    
+    override func mouseUp(with event: NSEvent) {
+        userIsAddingObject = false
+    }
+    
+    private var objects: [Object] { self.children.filter { $0.name == "object"} as! [Object] }
+    private var centerGravityObject: Object? { objects.filter { $0.isCenterObject}.first }
     
     private func initializeScene() {
         
@@ -75,27 +83,35 @@ class GameScene: SKScene {
         self.physicsWorld.speed = 1
         
         // Add center gravity object
-        addObject(
+        _ = addObject(
             mass: Constants.centerObjectMass,
             radius: 128,
             color: .white,
             isCenterObject: true
         )
         
-        // Add random objects
-        for _ in 0..<Constants.initialObjectCount {
-            addRandomObject()
-        }
-        
         // Initialize camera
         userCamera.position = .zero
         userCamera.xScale = 2
         userCamera.yScale = 2
         self.camera = userCamera
+        
+        // Add camera dragging
+        
     }
     
     override func didSimulatePhysics() {
         updateGravity()
+        
+        userCamera.position = centerGravityObject?.position ?? .zero
+        self.camera = userCamera
+        
+        removeDistantObjects()
+    }
+    
+    private func userAddObject() {
+        userIsAddingObject = true
+        newlyAddedObject = addRandomObject()
     }
     
     private func addObject(
@@ -106,7 +122,7 @@ class GameScene: SKScene {
         initialVelocity: CGVector = .zero,
         isDynamic: Bool = true,
         isCenterObject: Bool = false
-    ) {
+    ) -> Object {
         let object = Object(circleOfRadius: radius)
         object.position = position
         object.name = "object"
@@ -121,23 +137,66 @@ class GameScene: SKScene {
         object.radius = radius
         object.isCenterObject = isCenterObject
         self.addChild(object)
+        return object
     }
     
-    private func addRandomObject() {
+    private func addRandomObject() -> Object {
         let radius = CGFloat.random(in: 4...20)
         let mass = radius*25000
-        addObject(
+        return addObject(
             mass: mass,
             radius: radius,
             position: CGPoint(
                 x: CGFloat.random(in: Constants.objectsSpawnRangeX),
                 y: CGFloat.random(in: Constants.objectsSpawnRangeY)
             ),
-            initialVelocity: CGVector(
-                dx: CGFloat.random(in: Constants.initialSpeedRangeX),
-                dy: CGFloat.random(in: Constants.initialSpeedRangeY)
-            )
+            initialVelocity: .zero
         )
+    }
+    
+    private func removeDistantObjects() {
+        
+        let centerGravObjPos = centerGravityObject?.position ?? .zero
+        
+        objects
+            .filter { $0.position.distance(from: centerGravObjPos) >= 12000 }
+            .forEach { $0.removeFromParent() }
+    }
+    
+    private func updateVelocityIndicator() {
+        
+        let indicatorName = "velIndicator"
+        self.children.filter { $0.name == indicatorName }.forEach { $0.removeFromParent() }
+        guard newlyAddedObject != nil else { return }
+        
+        let objPos = newlyAddedObject!.position
+        let objVel = newlyAddedObject!.physicsBody!.velocity
+        let startPos = CGPoint(x: objPos.x, y: objPos.y)
+        let endPos = CGPoint(x: objPos.x + objVel.dx, y: objPos.y + objVel.dy)
+        let color = newlyAddedObject!.fillColor
+        let halfWidth = newlyAddedObject!.radius!
+        var points = [startPos, endPos]
+        
+        let indicator = SKShapeNode(
+            splinePoints: &points,
+            count: 2
+        )
+        let startIndicator = SKShapeNode(circleOfRadius: halfWidth)
+        startIndicator.position = startPos
+        startIndicator.fillColor = color
+        startIndicator.name = indicatorName
+        startIndicator.lineWidth = 0.0
+        let endIndicator = SKShapeNode(circleOfRadius: halfWidth)
+        endIndicator.position = endPos
+        endIndicator.fillColor = color
+        endIndicator.lineWidth = 0.0
+        indicator.strokeColor = color
+        indicator.lineWidth = halfWidth*2
+        indicator.name = indicatorName
+        indicator.isAntialiased = true
+        indicator.addChild(startIndicator)
+        indicator.addChild(endIndicator)
+        self.addChild(indicator)
     }
     
     private func updateGravity() {
@@ -146,8 +205,7 @@ class GameScene: SKScene {
             asyncGroup.enter()
             for objectB in objects {
                 
-                guard objectA.distance(from: objectB) != 0 else { continue }
-                guard !objectA.isCenterObject else { continue }
+                guard objectA.position.distance(from: objectB.position) <= 1000 || objectB.isCenterObject || objectA.isCenterObject else { continue }
                 
                 let acceleration : CGFloat = {
                     let G  = Constants.gravityConst
